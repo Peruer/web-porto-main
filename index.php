db46ef0dcedb11071f6d8543363322494fc06c83
Peruer <wirendilanadimulya@gmail.com>
2024-07-12 22:16:34 +0700

    tugas uas

---
+++ index.php
@@ -0,0 +1,433 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  <!--AOS-->
+
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+  <!--Akhir AOS-->
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+  <!--My CSS-->
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+  <title>Biodata | Frieren: Beyond Journey's End</title>
+</head>
+
+<body id="home">
+  <!-- Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-dark shadow fixed-top" style="background-color: #2e5fe7">
+    <div class="container">
+      <a class="navbar-brand" href="#">Frieren: Beyond Journey's End</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#sinopsis">Sinopsis</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Projects">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--Akhir Navbar-->
+
+  <!--Jumbotron-->
+  <section class="jumbotron text-center">
+    <img src="img/sousou-no-frieren.jpg" alt="Frieren" width="500" class="rounded-circle" />
+    <h1 class="fw-bold display-4">Frieren: Beyond Journey's End</h1>
+    <p class="fw-bold lead">Create By: Kanehito Yamada</p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,96L120,128C240,160,480,224,720,224C960,224,1200,160,1320,128L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!--Akhir Jumbotron-->
+
+  <!--Sinopsis-->
+  <section id="sinopsis">
+    <div class="container">
+      <div class="row text-center mb-4">
+        <div class="col">
+          <h2 class="fw-bold">Sinopsis Frieren: Beyond Journey's End</h2>
+        </div>
+      </div>
+      <div class="row fs-5 text-center">
+        <div class="col-md-5">
+          <p>
+            Sousou no Frieren adalah manga yang ditulis oleh Kanehito Yamada bersama Tsukasa Abe sebagai ilustratornya. Chapter pertamanya rilis pada 28 April 2020 lalu di Weekly Shonen Sunday. Sousou no Frieren merupakan manga yang cukup
+            populer, dibuktikan dengan raihan skor yang diperoleh melalui situs MAL, yakni 8,63
+          </p>
+        </div>
+        <div class="col-md">
+          <p>
+            Setelah berhasil mengalahkan raja iblis, kelompok pahlawan yang terdiri dari dwarf, elf, manusia dan pendeta kembali ke Kota untuk memulai hidup yang baru. Frieren, seorang elf telah berjanji ke rekannya untuk melihat hujan
+            meteor yang lebih indah. Suatu hari, Frieren mendatangi rekannya yakni Himmel, manusia yang turut mengalahkan raja iblis. Frieren terkejut ketika melihat Himmel kini telah menjadi kakek tua. Setelah melihat hujan meteor, sang
+            oahlawan manusia telah meninggal. Beberapa tahun berselang, rekan Frieren yakni sang pendeta juga turut menghembuskan napas terakhir. Menyadari bahwa elf memiliki umur panjang, Frieren mulai ingin membiasakan diri untuk tidak
+            melibatkan perasaan masa lalunya saat menjadi bagian dari kelompok pahlawan. Mampukah ia melakukannya?
+          </p>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#a2d9ff" fill-opacity="1" d="M0,0L120,48C240,96,480,192,720,218.7C960,245,1200,203,1320,181.3L1440,160L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!--Akhir Sinopsis-->
+
+  <!--Projects-->
+  <section id="Projects">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <h2 class="fw-bold">Projects</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/main-qimg-50cd22e4fa5c20beef706b7440a3d704.jpeg" class="card-img-top" alt="Projects1" />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/gambar-2-1.jpg" class="card-img-top" alt="Projects2" />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/foto manga.jpeg" class="card-img-top" alt="Projects3" />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#fff" fill-opacity="1" d="M0,160L48,160C96,160,192,160,288,181.3C384,203,480,245,576,250.7C672,256,768,224,864,186.7C960,149,1056,107,1152,106.7C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!--Akhir Projects-->
+
+  <!--Contact-->
+  <section id="contact">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <h2>Contact Me</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center mb-3">
+        <div class="col-md-8">
+          <form>
+            <div class="mb-3">
+              <label for="name" class="form-label">Nama Lengkap</label>
+              <input type="text" class="form-control" id="name" aria-describedby="name">
+
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" aria-describedby="email">
+
+            </div>
+            <div class="mb-3">
+              <label for="pesan" class="form-label">Pesan</label>
+              <textarea class="form-control" id="pesan" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Kirim</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#0d6efd" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,208C384,192,480,192,576,186.7C672,181,768,171,864,192C960,213,1056,267,1152,277.3C1248,288,1344,256,1392,240L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!--Akhir Contact-->
+
+  <!-- tabel -->
+  <section id="skills">
+    <div class="container">
+      <div id="message"></div>
+      <h1 class="mt-4 mb-4 text-center text-danger">USERS CRUD</h1>
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col col-sm-9">SKILLS</div>
+            <div class="col col-sm-3">
+              <button type="button" id="add_data" class="btn btn-success btn-sm float-end">Add</button>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered" id="sample_data">
+              <thead>
+                <tr>
+                  <th>User Id</th>
+                  <th>Skill Name</th>
+                  <th>Rating</th>
+                  <th>Description</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="modal" tabindex="-1" id="action_modal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <form method="post" id="sample_form">
+              <div class="modal-header">
+                <h5 class="modal-title" id="dynamic_modal_title"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div class="mb-3">
+                  <label class="form-label">User Id</label>
+                  <input type="text" name="user_id" id="user_id" class="form-control" />
+                  <span id="user_id_error" class="text-danger"></span>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Skill Name</label>
+                  <input type="skill_name" name="skill_name" id="skill_name" class="form-control" />
+                  <span id="skill_name_error" class="text-danger"></span>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Rating</label>
+                  <input type="rating" name="rating" id="rating" class="form-control" />
+                  <span id="rating_error" class="text-danger"></span>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Description</label>
+                  <input type="text" name="description" id="description" class="form-control" />
+                  <span id="description_error" class="text-danger"></span>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <input type="hidden" name="id" id="id" />
+                <input type="hidden" name="action" id="action" value="Add" />
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" id="action_button">Add</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end tabel -->
+  <!--Footer-->
+  <footer class="bg-primary text-white text-center p-2">
+    <p>Created with love bye <a href="https://www.instagram.com/lanadimulya_/" class="text-white fw-bold">Wirendi Lanadimulya</a></p>
+  </footer>
+  <!--Akhir Footer-->
+
+
+
+  <script>
+    $(document).ready(function() {
+      showAll();
+
+      $("#add_data").click(function() {
+        $("#dynamic_modal_title").text("Add Biodata User");
+        $("#sample_form")[0].reset();
+        $("#action").val("Add");
+        $("#action_button").text("Add");
+        $(".text-danger").text("");
+        $("#action_modal").modal("show");
+      });
+
+      $("#sample_form").on("submit", function(event) {
+        event.preventDefault();
+        if ($("#action").val() == "Add") {
+          var formData = {
+            user_id: $("#user_id").val(),
+            skill_name: $("#skill_name").val(),
+            rating: $("#rating").val(),
+            description: $("#description").val(),
+          };
+
+          $.ajax({
+            url: "https://wirendi.amisbudi.cloud/web-porto/si-admin/api/skills/create.php",
+            method: "POST",
+            data: JSON.stringify(formData),
+            success: function(data) {
+              $("#action_button").attr("disabled", false);
+              $("#message").html('<div class="alert alert-success">' + data.message + "</div>");
+              $("#action_modal").modal("hide");
+              $("#sample_data").DataTable().destroy();
+              showAll();
+            },
+            error: function(err) {
+              console.log(err);
+            },
+          });
+        } else if ($("#action").val() == "Update") {
+          var formData = {
+            id: $("#id").val(),
+            user_id: $("#user_id").val(),
+            skill_name: $("#skill_name").val(),
+            rating: $("#rating").val(),
+            description: $("#description").val(),
+          };
+
+          $.ajax({
+            url: "https://wirendi.amisbudi.cloud/web-porto/si-admin/api/skills/update.php",
+            method: "PUT",
+            data: JSON.stringify(formData),
+            success: function(data) {
+              $("#action_button").attr("disabled", false);
+              $("#message").html('<div class="alert alert-success">' + data.message + "</div>");
+              $("#action_modal").modal("hide");
+              $("#sample_data").DataTable().destroy();
+              showAll();
+            },
+            error: function(err) {
+              console.log(err);
+            },
+          });
+        }
+      });
+    });
+
+    function showAll() {
+      $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "https://wirendi.amisbudi.cloud/web-porto/si-admin/api/skills/read.php",
+        success: function(response) {
+          // console.log(response);
+          var json = response.body;
+          var dataSet = [];
+          for (var i = 0; i < json.length; i++) {
+            var sub_array = {
+              user_id: json[i].user_id,
+              skill_name: json[i].skill_name,
+              rating: json[i].rating,
+              description: json[i].description,
+              action: '<button onclick="showOne(' + json[i].id + ')" class="btn btn-sm btn-warning">Edit</button>' + '<button onclick="deleteOne(' + json[i].id + ')" class="btn btn-sm btn-danger mx-2">Delete</button>',
+            };
+            dataSet.push(sub_array);
+          }
+          $("#sample_data").DataTable({
+            data: dataSet,
+            columns: [{
+                data: "user_id",
+              },
+              {
+                data: "skill_name",
+              },
+              {
+                data: "rating",
+              },
+              {
+                data: "description",
+              },
+              {
+                data: "action",
+              },
+            ],
+          });
+        },
+        error: function(err) {
+          console.log(err);
+        },
+      });
+    }
+
+    function showOne(id) {
+      $("#dynamic_modal_title").text("Edit Biodata User");
+      $("#sample_form")[0].reset();
+      $("#action").val("Update");
+      $("#action_button").text("Update");
+      $(".text-danger").text("");
+      $("#action_modal").modal("show");
+
+      $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "https://wirendi.amisbudi.cloud/web-porto/si-admin/api/skills/read.php?id=" + id,
+        success: function(response) {
+          $("#id").val(response.id);
+          $("#user_id").val(response.user_id);
+          $("#skill_name").val(response.skill_name);
+          $("#rating").val(response.rating);
+          $("#description").val(response.description);
+        },
+        error: function(err) {
+          console.log(err);
+        },
+      });
+    }
+
+    function deleteOne(id) {
+      var konfirmasiUser = confirm("Yakin untuk hapus data ?");
+      if (konfirmasiUser) {
+        $.ajax({
+          url: "https://wirendi.amisbudi.cloud/web-porto/si-admin/api/skills/delete.php",
+          method: "DELETE",
+          data: JSON.stringify({
+            id: id,
+          }),
+          success: function(data) {
+            $("#action_button").attr("disabled", false);
+            $("#message").html('<div class="alert alert-success">' + data + "</div>");
+            $("#action_modal").modal("hide");
+            $("#sample_data").DataTable().destroy();
+            showAll();
+          },
+          error: function(err) {
+            console.log(err);
+            $("#message").html('<div class="alert alert-danger">' + err.responseJSON + "</div>");
+          },
+        });
+      }
+    }
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init({
+      duration: 1000,
+    });
+  </script>
+</body>
+
+</html>
